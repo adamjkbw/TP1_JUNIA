786bfea5255b6907cd6331184a00e5a18cad8224
Adam Jakubowski <adam.jakubowski@student.junia.com>
2025-11-03 15:23:01 +0100

    ex9

--- exec9.c
+++ exec9.c
@@ -3,6 +3,7 @@
 int main() {
     int a = 10;
     int b = 20;
-    printf("a=%d b=%d\n", b); 
+    printf("a=%d b=%d\n", a, b); 
     return 0;
 }
+/*on ne donne que b dans le printf. Pour fix on met aussi le a*/
