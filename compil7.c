83bd46f0192e677bb53bbc5790f33146b1eeb6e0
Adam Jakubowski <adam.jakubowski@student.junia.com>
2025-11-03 14:20:54 +0100

    push 2

--- compil7.c
+++ compil7.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
-// ici le comilateur ne dit malheureusement rien mais il devrait,
+// ici le compilateur ne dit malheureusement rien mais il devrait,
 // executer pour essayer de comprendre ce qu'il se passe 
 int main() {
     int tableau[5];
     int i = 12;
-    for (int j = 0; j < 5; j++)
+    for (int j = 0; j < 6; j++)
     {
         tableau[j] = 1;
     }
     printf("OK %d\n", tableau[5]);
     return 0;
 }
+
+/*Le problème était que on essayait d'afficher une valeur qui n'existe pas*/
