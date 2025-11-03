e2aa607aae5b6ab39d9b69182cf20f909ca63bce
Adam Jakubowski <adam.jakubowski@student.junia.com>
2025-11-03 13:33:48 +0100

    push compil

---
+++ compil1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello world\n")
+    return 0;
+}
+
+/*compil1.c: In function 'main':
+compil1.c:5:5: error: expected ';' before 'return'
+     return 0;
+     ^~~~~~
+     
+Le problème c'et que il manque un point virgule après le print*/
