49a99dbf8244719d34909c6f47efc156e63644ef
Adam Jakubowski <adam.jakubowski@student.junia.com>
2025-11-03 15:20:10 +0100

    ex8

--- exec8.c
+++ exec8.c
@@ -2,7 +2,9 @@
 
 int main() {
     int t[3] = {10, 20, 30};
-    int i = -1;
+    int i = 2;
     printf("%d\n", t[i]); 
     return 0;
 }
+
+/*la valeur de i est négative, les indexes d'un tableau commencent à 0*/
