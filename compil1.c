46ef6b1bbb13579c6b4cca7731f602cb6dcfd8bb
Bordelais <louisbordelais@outlook.fr>
2025-11-03 13:55:00 +0100

    test

--- compil1.c
+++ compil1.c
@@ -4,3 +4,8 @@ int main() {
     printf("Hello world\n");
     return 0;
 }
+/* compil1.c:5:5: error: expected ';' before 'return'
+     return 0;
+     ^~~~~~
+     
+Le problème c'et que il manque un point virgule après le print*/
