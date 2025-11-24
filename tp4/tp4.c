ebcf9cca041b99829dddb1ec2729f4c5aadca27d
Adam Jakubowski <adam.jakubowski@student.junia.com>
2025-11-24 15:15:31 +0100

    Etape 6

--- tp4/tp4.c
+++ tp4/tp4.c
@@ -16,31 +16,86 @@ int lireChoix()
 {
     int choix;
     printf("Votre choix : ");
-    scanf("%d\n", &choix);
+    scanf("%d", &choix);
     return choix;
 }
 
 int saisirNombreEleves()
 {
     int nbEleves = 0;
-    while (nbEleves > 30 && nbEleves < 1)
+    printf("Entrez le nombre d'eleves (1 a 30) : ");
+    scanf("%d", &nbEleves);
+    if (nbEleves >= 1 && nbEleves <= 30)
     {
-        printf("Entrez le nombre d'eleves (1 a 30) : ");
-        scanf("%d\n", nbEleves);
-        if (nbEleves > 30 && nbEleves < 1)
+        return nbEleves;
+    }
+    else
+    {
+        printf("valeur invalide\n");
+        saisirNombreEleves();
+    }
+}
+
+void saisirNotes(int note[][3], int n)
+{
+    printf("Saisie des notes pour %d eleves et 3 controles.\n", n);
+    for (int i = 0; i < n; i++)
+    {
+        printf("Eleve %d :\n", i + 1);
+        printf("  Note du controle 1 (0 a 20) : ");
+        scanf("%d", &note[i][0]);
+        printf("  Note du controle 2 (0 a 20) : ");
+        scanf("%d", &note[i][1]);
+        printf("  Note du controle 3 (0 a 20) : ");
+        scanf("%d", &note[i][2]);
+    }
+}
+
+void afficherNotes(int notes[][3], int n)
+{
+    printf("Tableau des notes\n");
+    printf("Eleve ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("C%d ", i + 1);
+    }
+    printf("\n");
+    for (int i = 0; i < 3; i++)
+    {
+        printf("   %d  ", i + 1);
+        for (int j = 0; j < n; j++)
         {
-            printf("Valeur invalide");
+            printf(" %d ", notes[j][i]);
         }
-        else
-        {
-            return nbEleves;
-        }
+        printf("\n");
+    }
+}
+
+float calculerMoyenneEleve(int notes[][3], int c)
+{
+    return ((float)notes[c][0] + (float)notes[c][1] + (float)notes[c][2]) / 3.0;
+}
+
+float calculerMoyenneGenerale(n)
+{
+    for (int i = 0; i < n; i++)
+    {
     }
 }
 
 int main()
 {
+    int nbEleves = 0;
     afficherMenu();
     lireChoix();
+    nbEleves = saisirNombreEleves();
+    int notes[nbEleves][3];
+    saisirNotes(notes, nbEleves);
+    afficherNotes(notes, nbEleves);
+    int eleveChoisi;
+    printf("Entrez l'indice de l'eleve (1 a %d): ", nbEleves);
+    scanf("%d", &eleveChoisi);
+    printf("Moyenne de l'eleve %d: %f", eleveChoisi, calculerMoyenneEleve(notes, eleveChoisi - 1));
+
     return 0;
 }
